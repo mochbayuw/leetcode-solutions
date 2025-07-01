c90ca9c444db70a28021f1df89592d7ca56a096c
mochbayuw <mochamadbayuw@gmail.com>
2025-07-01 08:56:48 +0700

    nomor 4 leetcode

---
+++ problems/4-median-of-two-sorted-arrays.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    const merged = [...nums1, ...nums2].sort((a, b) => a - b);
+    const len = merged.length;
+    const mid = Math.floor(len / 2);
+
+    if (len % 2 === 0) {
+        return (merged[mid - 1] + merged[mid]) / 2;
+    } else {
+        return merged[mid];
+    }
+};
